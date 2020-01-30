153148fe40357dc428bf2ca260a1b2a6c20e00b8
Pasante <Pasante@CSID.LOCAL>
2020-01-30 15:23:32 -0400

    initial commit

---
+++ Bucles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++ )
+            {
+                if(number % 3 == 0 && number % 5 == 0){
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if ( number % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
